f6cf7d30f6dcec440c4c34114cfa26a1fb055047
Samuel <samli177@student.liu.se>
2012-04-16 15:31:30 +0200

    wee

---
+++ init.rkt
@@ -0,0 +1,28 @@
+;---superclass for object that can be displayed on screen as sprite---
+(define on-screen% 
+  
+  (class object%
+    (super-new)
+    
+    (field (x 0) (y 0)   ;; possition of object on screen 
+           (sx 0) (sy 0) ;; space occupied by object on screen
+           (sprite ""))  ;; path to image representing object on screen
+    
+    ;;--------------get-methods-------------------
+    (define/public (get-x) x)
+    (define/public (get-y) y)
+    (define/public (get-sx) sx)
+    (define/public (get-sy) sy)
+    (define/public (get-sprite) sprite)
+    ;---------------set-methods-------------------
+    (define/public (set-xy! new-x new-y) (begin (set! x new-x) (set! y new-y)))
+    (define/public (set-x! new-x) (set! x new-x))
+    (define/public (set-y! new-y) (set! y new-y))
+    (define/public (set-sx! new-sx) (set! sx new-sx))
+    (define/public (set-sy! new-sy) (set! sy new-sy))
+    (define/public (set-sprite! new-sprite) (set! sprite new-sprite))))
+    
+    
+;---------testing--------------
+(define *test-osc* (new on-screen%))
+(send *test-osc* set-xy! 1 2)
