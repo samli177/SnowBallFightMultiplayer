d4cc4dbfb3d4ef33426cb049899b28a5d47acb3b
Benjamin Lind <extramailb@gmail.com>
2012-05-15 20:49:09 +0200

    =)

--- init.rkt
+++ init.rkt
@@ -166,7 +166,7 @@
 
 (define *player*
   (new player% 
-       [sprite (make-object bitmap% "red_player.png" 'png/alpha #f)]
+       [sprite (make-object bitmap% "blue_player.png" 'png/alpha #f)]
        [powerbar (new powerbar% [sprite (make-object bitmap% "kraft0.png" 'png/alpha #f)])]))
 
 (send *player* set-radius! (round (/ (send (send *player* get-sprite) get-height) 2)))
